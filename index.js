b4f313a04c9b12ad5ac1362a76c7c8ef998a03ab
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-23 12:09:59 +0530

    Fix- negative values bug fixed

--- index.js
+++ index.js
@@ -2,37 +2,23 @@ const initialPrice = document.querySelector("#initial-price");
 const quantity = document.querySelector("#quantity");
 const currentPrice = document.querySelector("#current-price");
 const showBtn = document.querySelector("#show-btn");
-const output = document.querySelector("output");
+const output = document.querySelector("#output");
 
-function calculateProfitAndLoss(cp, sp, quantity) {
-    if (sp > cp) {
-        const profit = (sp - cp) * quantity;
-        return profit;
-    } else {
-        const loss = (cp - sp) * quantity;
-        return loss;
+function calculateProfitAndLoss(initial, current, quantity) {
+    if (current > initial) {
+        const profit = (current - initial) * quantity;
+        const profitPercent = (profit / initial) * 100;
+        output.innerText = `Hey! the profit is ${profit.toFixed(2)} and the percentage is ${profitPercent.toFixed(2)}%`;
+    } else if(current < initial){
+        const loss = (initial - current) * quantity;
+        const lossPercent = (loss / initial) * 100;
+        output.innerText = `Hey! the loss is ${loss.toFixed(2)} and percentage is ${lossPercent.toFixed(2)}%`;
+    }
+    else{
+        output.innerText = `No profit and No loss`;
     }
 }
-function calculateProfitAndLossPercentages(cp, sp) {
-    if (sp > cp) {
-        const profit = calculateProfitAndLoss(initialPrice.value, currentPrice.value, quantity.value);
-        const profitPercent = (profit / cp) * 100;
-        return profitPercent.toFixed(2);
-    } else {
-        const loss = calculateProfitAndLoss(initialPrice.value, currentPrice.value, quantity.value);
-        const lossPercent = (loss / cp) * 100;
-        return lossPercent.toFixed(2);
-    }
-}
-
-
 function clickHandler() {
-    const profitOrLoss = calculateProfitAndLoss(initialPrice.value, currentPrice.value, quantity.value);
-    const profitOrLossPercent = calculateProfitAndLossPercentages(initialPrice.value, currentPrice.value);
-    if(initialPrice.value < currentPrice.value){
-        output.innerText = `profit: ${profitOrLoss} and profitpercent: ${profitOrLossPercent}`;
-    }else{
-        output.innerText = `loss: ${profitOrLoss} and losspercent: ${profitOrLossPercent}`;
-    }
+    calculateProfitAndLoss(Number(initialPrice.value),Number(currentPrice.value),Number(quantity.value));
 }
 showBtn.addEventListener("click", clickHandler);
